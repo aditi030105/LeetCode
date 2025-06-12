66d68bb092ad7cfaf7226f79670563b6e1748213
aditi030105 <aditisinharoy0301@gmail.com>
2025-06-12 19:42:06 +0530

    Time: 31 ms (76.85%) | Memory: 60.8 MB (41.02%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums){
+            list.add(num);
+        }
+        return quickSelect(list, k);
+    }
+    private int quickSelect(List<Integer> nums, int k){
+        if(nums.size() == 1) return nums.get(0);
+        int pivotIndex = new Random().nextInt(nums.size());
+        int pivot = nums.get(pivotIndex);
+
+        List<Integer> less = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
+
+        for(int num : nums){
+            if(num > pivot){
+                greater.add(num);
+            }else if(num < pivot){
+                less.add(num);
+            }else{
+                equal.add(num);
+            }
+        }
+        if(k <= greater.size()){
+            return quickSelect(greater, k);
+        }
+        if(greater.size() + equal.size() < k){
+            return quickSelect(less, k - greater.size() - equal.size());
+        }
+        return pivot;
+    }
+}
