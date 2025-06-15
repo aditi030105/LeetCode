0be27fea360e6d70308327d69851b12144a1ad74
aditi030105 <aditisinharoy0301@gmail.com>
2025-06-15 22:22:24 +0530

    Time: 9 ms (99.18%) | Memory: 60.6 MB (16.66%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<Integer>();
+        for(int i : nums){
+            if(s.add(i)){
+                continue;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
