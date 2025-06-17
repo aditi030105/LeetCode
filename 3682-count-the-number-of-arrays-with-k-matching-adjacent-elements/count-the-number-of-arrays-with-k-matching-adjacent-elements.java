86e98efcfba75ee1f380530da36aabab6bb809db
aditi030105 <aditisinharoy0301@gmail.com>
2025-06-17 23:24:37 +0530

    Time: 29 ms (65.85%) | Memory: 42.2 MB (65.85%) - LeetSync

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.java
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.java
@@ -1,43 +1,40 @@
 class Solution {
     static final int MOD = (int)1e9 + 7;
-    static long[] fact, invFact;
+    static final int MX = 100000;
+    static long[] fact = new long[MX];
+    static long[] invFact = new long[MX];
 
-    static long qpow(long x, int n) {
-        long result = 1;
-        x = x % MOD;
-        while (n > 0) {
-            if ((n & 1) == 1)
-                result = (result * x) % MOD;
-            x = (x * x) % MOD;
-            n >>= 1;
+    static{
+        fact[0] = 1;
+        for(int i = 1; i < MX; i++){
+            fact[i] = (fact[i-1] * i) % MOD;
+        }
+        invFact[MX - 1] = qpow(fact[MX-1], MOD-2);
+        for(int i = MX - 1; i > 0; i--){
+            invFact[i-1] = (invFact[i] * i) % MOD;
+        }
+    }
+
+    static long qpow(long x, int n){
+        if(n == 0) return 1;
+        long half = qpow(x, n/2);
+        long result = (half * half) % MOD;
+        if(n%2 == 1){
+            result = (result * x) % MOD;
         }
         return result;
     }
 
-    static void init(int n) {
-        fact = new long[n + 1];
-        invFact = new long[n + 1];
-        fact[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            fact[i] = (fact[i - 1] * i) % MOD;
-        }
-        invFact[n] = qpow(fact[n], MOD - 2);
-        for (int i = n - 1; i >= 0; i--) {
-            invFact[i] = (invFact[i + 1] * (i + 1)) % MOD;
-        }
-    }
-
-    long comb(int n, int r) {
-        if (r < 0 || r > n) return 0;
-        return (((fact[n] * invFact[r]) % MOD) * invFact[n - r]) % MOD;
+    long comb(int n, int r){
+        return(((fact[n] * invFact[r]) % MOD) * invFact[n-r]) %MOD;
     }
 
     public int countGoodArrays(int n, int m, int k) {
-        init(n); // Dynamic size
-        long equalPos = comb(n - 1, k);
-        long firstSeg = m;
-        long remaining = qpow(m - 1, n - k - 1);
-        long result = equalPos * firstSeg % MOD * remaining % MOD;
-        return (int) result;
+        long waysToChooseEqualPositions = comb(n-1, k);
+        long firstSegmentChoices = m;
+        long remainingSegmentChoices = qpow(m-1, n-k-1);
+        long result = (waysToChooseEqualPositions * firstSegmentChoices) % MOD;
+        result = (result * remainingSegmentChoices) % MOD;
+        return (int)result;
     }
 }
