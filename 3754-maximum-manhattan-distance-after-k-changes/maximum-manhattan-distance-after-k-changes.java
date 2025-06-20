dfe1602605f7f171ae62dbe478d997c07251107c
aditi030105 <aditisinharoy0301@gmail.com>
2025-06-20 21:51:59 +0530

    Time: 47 ms (100.00%) | Memory: 45.4 MB (95.45%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
@@ -1,36 +1,15 @@
 class Solution {
     public int maxDistance(String s, int k) {
-        int ans = 0;
+        int ans = 0, step = 0, longitude = 0, latitude = 0;
+        for(char c : s.toCharArray()){
+            if(c == 'N') latitude++;
+            else if(c == 'S') latitude--;
+            else if(c == 'E') longitude++;
+            else if(c == 'W') longitude--;
+            step++;
 
-        for (char hor : new char[]{'E', 'W'}) {
-            for (char ver : new char[]{'N', 'S'}) {
-                int remainingK = k;
-                int netX = 0, netY = 0;
-
-                for (int i = 0; i < s.length(); i++) {
-                    char ch = s.charAt(i);
-
-                    if (remainingK > 0 && (ch == hor)) {
-                        ch = (ch == 'W') ? 'E' : 'W';
-                        remainingK--;
-                    }
-                    else if (remainingK > 0 && (ch == ver)) {
-                        ch = (ch == 'N') ? 'S' : 'N';
-                        remainingK--;
-                    }
-
-                    switch (ch) {
-                        case 'E': netX++; break;
-                        case 'W': netX--; break;
-                        case 'N': netY++; break;
-                        case 'S': netY--; break;
-                    }
-
-                    ans = Math.max(ans, Math.abs(netX) + Math.abs(netY));
-                }
-            }
+            ans = Math.max(ans, Math.min(Math.abs(latitude) + Math.abs(longitude) + k * 2, step));
         }
-
         return ans;
     }
 }
