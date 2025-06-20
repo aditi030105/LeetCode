b69702e264d524e4e9dc7e7a3c4da64859a9d265
aditi030105 <aditisinharoy0301@gmail.com>
2025-06-20 06:52:00 +0530

    Time: 217 ms (24.24%) | Memory: 46 MB (31.82%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
@@ -1,27 +1,36 @@
 class Solution {
     public int maxDistance(String s, int k) {
         int ans = 0;
+
         for (char hor : new char[]{'E', 'W'}) {
             for (char ver : new char[]{'N', 'S'}) {
-                int remainingK = k, netX = 0, netY = 0;
+                int remainingK = k;
+                int netX = 0, netY = 0;
+
                 for (int i = 0; i < s.length(); i++) {
                     char ch = s.charAt(i);
-                    if (ch == hor && remainingK > 0) {
+
+                    if (remainingK > 0 && (ch == hor)) {
                         ch = (ch == 'W') ? 'E' : 'W';
                         remainingK--;
-                    } else if (ch == ver && remainingK > 0) {
+                    }
+                    else if (remainingK > 0 && (ch == ver)) {
                         ch = (ch == 'N') ? 'S' : 'N';
                         remainingK--;
                     }
-                    if (ch == 'E') netX++;
-                    if (ch == 'W') netX--;
-                    if (ch == 'N') netY++;
-                    if (ch == 'S') netY--;
+
+                    switch (ch) {
+                        case 'E': netX++; break;
+                        case 'W': netX--; break;
+                        case 'N': netY++; break;
+                        case 'S': netY--; break;
+                    }
 
                     ans = Math.max(ans, Math.abs(netX) + Math.abs(netY));
                 }
             }
         }
+
         return ans;
     }
 }
