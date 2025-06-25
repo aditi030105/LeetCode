5387c83afd8976ece055c9cd9f4a7b469a347e56
aditi030105 <aditisinharoy0301@gmail.com>
2025-06-25 22:06:04 +0530

    Time: 700 ms (60.08%) | Memory: 55.6 MB (28.68%) - LeetSync

--- 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.java
+++ 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.java
@@ -1,58 +1,48 @@
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        long left = -1_000_000_0000L; // -1e10
-        long right = 1_000_000_0000L; // 1e10
-        long result = 0;
+        long low = -100_000L * 100_000L;
+        long high = 100_000L * 100_000L;
 
-        while (left <= right) {
-            long midProduct = left + (right - left) / 2;
-            long count = countLessEqual(nums1, nums2, midProduct);
-
-            if (count >= k) {
-                result = midProduct;
-                right = midProduct - 1;
-            } else {
-                left = midProduct + 1;
+        while (low < high) {
+            long mid = low + (high - low)/2;
+            if(countPairsLessEqual(nums1, nums2, mid) < k){
+                low = mid + 1;
+            }else{
+                high = mid;
             }
         }
-
-        return result;
+        return low;
     }
 
-    private long countLessEqual(int[] nums1, int[] nums2, long midProduct) {
+    private long countPairsLessEqual(int[] nums1, int[] nums2, long target) {
         long count = 0;
-        int n = nums2.length;
-
-        for (int a : nums1) {
-            if (a >= 0) {
-                int l = 0, r = n - 1, pos = -1;
-                while (l <= r) {
-                    int m = l + (r - l) / 2;
-                    long product = 1L * a * nums2[m];
-                    if (product <= midProduct) {
-                        pos = m;
-                        l = m + 1;
-                    } else {
-                        r = m - 1;
+        for(int a : nums1){
+            if(a > 0){
+                int left = 0, right = nums2.length;
+                while(left < right){
+                    int mid = left + (right - left)/2;
+                    if((long)a * nums2[mid] <= target){
+                        left = mid + 1;
+                    }else{
+                        right = mid;
                     }
                 }
-                count += (pos + 1);
-            } else {
-                int l = 0, r = n - 1, pos = n;
-                while (l <= r) {
-                    int m = l + (r - l) / 2;
-                    long product = 1L * a * nums2[m];
-                    if (product <= midProduct) {
-                        pos = m;
-                        r = m - 1;
-                    } else {
-                        l = m + 1;
+                count += left;
+            }else if(a < 0){
+                int left = 0, right = nums2.length;
+                while(left < right){
+                    int mid = left + (right - left)/2;
+                    if((long)a * nums2[mid] <= target){
+                        right = mid;
+                    }else{
+                        left = mid + 1;
                     }
                 }
-                count += (n - pos);
+                count += nums2.length - left;
+            }else{
+                if(target >= 0) count += nums2.length;
             }
         }
-
         return count;
     }
 }
