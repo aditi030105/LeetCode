6f57eb3a78aeeb4ec48ce771f1fa48602db3a96e
aditi030105 <aditisinharoy0301@gmail.com>
2025-06-29 20:36:28 +0530

    Time: 27 ms (99.52%) | Memory: 55.2 MB (53.38%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,29 +1,23 @@
 class Solution {
-    final int MOD = (int)1e9 + 7;
-    private void precomputePowerOfTwo(int[] powerOfTwo, int n){
-        powerOfTwo[0] = 1;
-        for (int i = 1; i < n; ++i) {
-            powerOfTwo[i] = (int)((powerOfTwo[i - 1] * 2L) % MOD);
-        }
-    }
+    private static final int MOD = 1_000_000_007;
     public int numSubseq(int[] nums, int target) {
-        int n = nums.length;
-        int[] powerOfTwo = new int[n];
-        precomputePowerOfTwo(powerOfTwo, n);
-        
         Arrays.sort(nums);
-        int subsequences = 0;
-        
-        int left = 0;
-        int right = n - 1;
-        while (left <= right) {
-            if (nums[left] + nums[right] > target) {
-                right--;
-            } else {
-                subsequences = (subsequences + powerOfTwo[right - left]) % MOD;
+        int n = nums.length;
+        int[] pow2 = new int[n];
+        pow2[0] = 1;
+        for(int i = 1; i < n; i++){
+            pow2[i] = (pow2[i-1] * 2) % MOD;
+        }
+        int left = 0, right = n - 1;
+        int count = 0;
+        while(left <= right){
+            if(nums[left] + nums[right] <= target){
+                count = (count + pow2[right - left]) % MOD;
                 left++;
+            }else{
+                right--;
             }
         }
-        return subsequences;
+        return count;
     }
 }
