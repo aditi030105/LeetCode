edbb170841f6fb53392120f8522e34dde4d7831d
aditi030105 <aditisinharoy0301@gmail.com>
2025-06-30 13:19:39 +0530

    Time: 1 ms (93.62%) | Memory: 46.2 MB (6.26%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -17,7 +17,7 @@ class Solution {
             return null;
         }
         ListNode current = head;
-        while (current.next != null) {
+        while (current != null && current.next != null) {
             if (current.next.val == val) {
                 current.next = current.next.next;
             } else {
