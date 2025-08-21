40347f81bdbb48671d3ee6b54d766c7190156330
aditi030105 <aditisinharoy0301@gmail.com>
2025-08-21 10:50:02 +0530

    Time: 14 ms (15.27%) | Memory: 45.1 MB (16.99%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+                String rowKey = c + "@row" + i;
+                String colKey = c + "@col" + j;
+                String boxKey = c + "@box" + (i / 3) + (j / 3);
+                
+                if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(boxKey)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
