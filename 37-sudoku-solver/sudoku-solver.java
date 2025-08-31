0002e3915193548f8e9b40670e4c9bc341e94854
aditi030105 <aditisinharoy0301@gmail.com>
2025-08-31 20:58:00 +0530

    Time: 204 ms (65.01%) | Memory: 40.8 MB (98.01%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,68 +1,47 @@
-import java.util.*;
-
 class Solution {
-    int[] rows = new int[9];
-    int[] cols = new int[9];
-    int[] boxes = new int[9];
-    List<int[]> empties = new ArrayList<>();
-
+    
     public void solveSudoku(char[][] board) {
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                if (board[r][c] == '.') {
-                    empties.add(new int[]{r, c});
-                } else {
-                    int num = board[r][c] - '0';
-                    int mask = 1 << num;
-                    int box = (r / 3) * 3 + (c / 3);
-                    rows[r] |= mask;
-                    cols[c] |= mask;
-                    boxes[box] |= mask;
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char d = '1'; d <= '9'; d++) {
+                        if (isValid(board, i, j, d)) {
+                            board[i][j] = d;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
                 }
             }
         }
-        backtrack(board, 0);
+        return true;
     }
 
-    private boolean backtrack(char[][] board, int idx) {
-        if (idx == empties.size()) return true;
+    private boolean isValid(char[][] board, int row, int col, char d) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == d) return false;
+            if (board[row][i] == d) return false;
+        }
 
-        int minIdx = idx, minCount = 10;
-        for (int i = idx; i < empties.size(); i++) {
-            int r = empties.get(i)[0];
-            int c = empties.get(i)[1];
-            int box = (r / 3) * 3 + (c / 3);
-            int used = rows[r] | cols[c] | boxes[box];
-            int candidates = 9 - Integer.bitCount(used);
-            if (candidates < minCount) {
-                minCount = candidates;
-                minIdx = i;
-                if (minCount == 1) break;
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
+                if (board[startRow + k][startCol + l] == d) {
+                    return false;
+                }
             }
         }
-        Collections.swap(empties, idx, minIdx);
-
-        int r = empties.get(idx)[0];
-        int c = empties.get(idx)[1];
-        int box = (r / 3) * 3 + (c / 3);
-        int used = rows[r] | cols[c] | boxes[box];
-
-        for (int num = 1; num <= 9; num++) {
-            int mask = 1 << num;
-            if ((used & mask) == 0) {
-                board[r][c] = (char) (num + '0');
-                rows[r] |= mask;
-                cols[c] |= mask;
-                boxes[box] |= mask;
-
-                if (backtrack(board, idx + 1)) return true;
-
-                board[r][c] = '.';
-                rows[r] ^= mask;
-                cols[c] ^= mask;
-                boxes[box] ^= mask;
-            }
-        }
-        return false;
+        return true;
     }
 }
