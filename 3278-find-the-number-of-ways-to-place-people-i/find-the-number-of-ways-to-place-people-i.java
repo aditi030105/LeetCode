509fd592a23a2093c1035c5d5efdb594533f5439
aditi030105 <aditisinharoy0301@gmail.com>
2025-09-02 23:27:31 +0530

    Time: 5 ms (100.00%) | Memory: 44.1 MB (100.00%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
@@ -1,34 +1,20 @@
 class Solution {
-    public int numberOfPairs(int[][] points) {
-        int n = points.length;
-
-        Arrays.sort(points, (point1, point2) -> {
-            if (point1[0] == point2[0]) {
-                return point2[1] - point1[1];
-            }
-            return point1[0] - point2[0];
-        });
-
-        int result = 0;
-
-        for (int i = 0; i < n; i++) {
-            int x1 = points[i][0];
-            int y1 = points[i][1];
-
-            int bestY = Integer.MIN_VALUE;
-
-            for (int j = i + 1; j < n; j++) {
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-                if (y2 > y1) continue;
-
-                if (y2 > bestY) {
-                    result++;
-                    bestY = y2;
-                }
+   public int numberOfPairs(int[][] points) {
+    Arrays.sort(points, (a, b) -> a[0] == b[0] ? 
+        b[1] - a[1] : a[0] - b[0]);
+    int cnt = 0;
+    for (int i = 0; i < points.length; i++) {
+        int x0 = points[i][0], y0 = points[i][1];
+        int bot = Integer.MIN_VALUE, top = y0;
+        for (int j = i + 1; j < points.length; j++) {
+            int x1 = points[j][0], y1 = points[j][1];
+            if (y1 <= top && y1 > bot) {
+                cnt++;
+                bot = y1;
+                if (y1 == top) top--;
             }
         }
-
-        return result;
     }
+    return cnt;
+}
 }
