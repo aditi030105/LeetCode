1e870358d0df3cf5f53f0806e0a79405120df577
aditi030105 <aditisinharoy0301@gmail.com>
2025-10-07 22:26:08 +0530

    Time: 23 ms (99.54%) | Memory: 62.8 MB (26.03%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
@@ -1,31 +1,39 @@
 class Solution {
     public int[] avoidFlood(int[] rains) {
-        int N = rains.length;
-        int[] ans = new int[N];
-
-        Map<Integer, Integer> fullMap = new HashMap<>();
-
-        TreeSet<Integer> zeroSet = new TreeSet<>();
-        for ( int ii = 0; ii < N; ii++ ) {
-            int lake = rains[ii];
-            if ( lake == 0 ) {
-                zeroSet.add(ii);
-                ans[ii] = 1;
-            }
-            else {
-                ans[ii] = -1;
-
-                Integer idx = fullMap.get(lake);
-                if ( idx != null ) {
-                    Integer zeroIdx = zeroSet.ceiling(idx);
-                    if ( zeroIdx == null ) return new int[0];
-                    zeroSet.remove(zeroIdx);
-                    ans[zeroIdx] = lake;
-                }
-                fullMap.put(lake, ii);
-            }
+        int n = rains.length;
+        int[] fa = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            fa[i] = i;
         }
 
+        int[] ans = new int[n];
+        Map<Integer, Integer> fullDay = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int lake = rains[i];
+            if (lake == 0) {
+                ans[i] = 1;
+                continue;
+            }
+            Integer j = fullDay.get(lake);
+            if (j != null) {
+                int dryDay = find(j + 1, fa);
+                if (dryDay >= i) {
+                    return new int[]{};
+                }
+                ans[dryDay] = lake;
+                fa[dryDay] = find(dryDay + 1, fa);
+            }
+            ans[i] = -1;
+            fa[i] = i + 1;
+            fullDay.put(lake, i);
+        }
         return ans;
+    }
+
+    private int find(int x, int[] fa) {
+        if (fa[x] != x) {
+            fa[x] = find(fa[x], fa);
+        }
+        return fa[x];
     }
 }
